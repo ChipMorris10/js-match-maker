d9ef2a6311f3365645869226c5a393b341f5c315
ChipMorris10 <goldcomac@mac.com>
2015-08-06 13:34:08 -0600

    discard these changes

--- main.js
+++ main.js
@@ -15,14 +15,14 @@ function capitalizeFirstLetter(string) {
 // collect studentName, studentPhone, studentCity
 var totalStudents = [];
   for(i = 0; i < numStudents; i++){
-    var studentName = prompt("What is the student's name? ");
-    var studentPhoneNo = prompt("What is the student's phone number? (xxx-xxx-xxxx)");
+  var studentName = prompt("What is the student's name? ");
+  var studentPhoneNo = prompt("What is the student's phone number? (xxx-xxx-xxxx)");
 
-// while the phone number doesn't comply to format, prompt re-enter
-while (studentPhoneNo.length != 12 || studentPhoneNo.charAt(3) != "-" || studentPhoneNo.charAt(7) != "-")  {
-  alert(" Your phone number is not valid, please re-enter your number using the format xxx-xxx-xxxx ");
-  var studentPhoneNo = prompt("What is the student's phone number? (xxx-xxx-xxxx");
-}
+  // while the phone number doesn't comply to format, prompt re-enter
+  while (studentPhoneNo.length != 12 || studentPhoneNo.charAt(3) != "-" || studentPhoneNo.charAt(7) != "-")  {
+    alert(" Your phone number is not valid, please re-enter your number using the format xxx-xxx-xxxx ");
+    var studentPhoneNo = prompt("What is the student's phone number? (xxx-xxx-xxxx");
+  }
 
   var studentCity = prompt("What city does the student live in?");
   var studentInfo = {name: studentName, number: studentPhoneNo, city:studentCity};
@@ -36,6 +36,41 @@ for (var i = 0; i < totalStudents.length; i ++) {
 
 }
 console.log(totalStudents);
+
+// ---------- M E N T O R S ----------
+
+// var numMentors = parseInt(prompt("How many mentors are teaching JavaScript?"));
+
+// function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+
+
+// // create empty array for totalMentors
+// // collect mentorName, mentorPhone, mentorCity
+// var totalMentors = [];
+//   for(i = 0; i < numMentors; i++){
+//   var mentorName = prompt("What is the mentor's name? ");
+//   var mentorPhoneNo = prompt("What is the mentor's phone number? (xxx-xxx-xxxx)");
+
+//   // while the phone number doesn't comply to format, prompt re-enter
+//   while (mentorPhoneNo.length != 12 || mentorPhoneNo.charAt(3) != "-" || mentorPhoneNo.charAt(7) != "-")  {
+//     alert(" Your phone number is not valid, please re-enter your number using the format xxx-xxx-xxxx ");
+//     var mentorPhoneNo = prompt("What is the mentor's phone number? (xxx-xxx-xxxx");
+//   }
+
+//   var mentorCity = prompt("What city does the mentor live in?");
+//   var mentorInfo = {name: mentorName, number: mentorPhoneNo, city:mentorCity};
+
+//   totalMentors.push(mentorInfo);
+// }
+
+// for (var i = 0; i < totalMentors.length; i ++) {
+
+//   alert(" Name: " + capitalizeFirstLetter(totalMentors[i].name) + " \n Phone: " + totalMentors[i].number + "\n City: " + totalMentors[i].city.toUpperCase());
+
+// }
+// console.log(totalMentors);
 
 
 
